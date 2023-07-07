b3ffd6b773be7b28b7abfb6bb38199a934e13a3c
Shyamal <scghoshbc@gmail.com>
2023-07-07 14:48:36 -0700

    committed

--- library.js
+++ library.js
@@ -1,88 +1,130 @@
 const library = {
-  tracks: { t01: { id: "t01",
-                   name: "Code Monkey",
-                   artist: "Jonathan Coulton",
-                   album: "Thing a Week Three" },
-            t02: { id: "t02",
-                   name: "Model View Controller",
-                   artist: "James Dempsey",
-                   album: "WWDC 2003"},
-            t03: { id: "t03",
-                   name: "Four Thirty-Three",
-                   artist: "John Cage",
-                   album: "Woodstock 1952"}
-          },
-  playlists: { p01: { id: "p01",
-                      name: "Coding Music",
-                      tracks: ["t01", "t02"]
-                    },
-               p02: { id: "p02",
-                      name: "Other Playlist",
-                      tracks: ["t03"]
-                    }
-             }
+       tracks: {
+              t01: {
+                     id: "t01",
+                     name: "Code Monkey",
+                     artist: "Jonathan Coulton",
+                     album: "Thing a Week Three"
+              },
+              t02: {
+                     id: "t02",
+                     name: "Model View Controller",
+                     artist: "James Dempsey",
+                     album: "WWDC 2003"
+              },
+              t03: {
+                     id: "t03",
+                     name: "Four Thirty-Three",
+                     artist: "John Cage",
+                     album: "Woodstock 1952"
+              }
+       },
+       playlists: {
+              p01: {
+                     id: "p01",
+                     name: "Coding Music",
+                     tracks: ["t01", "t02"]
+              },
+              p02: {
+                     id: "p02",
+                     name: "Other Playlist",
+                     tracks: ["t03"]
+              }
+       }
 };
 
-/////////////////////////////
-// FUNCTIONS TO IMPLEMENT:
-/////////////////////////////
-
-// prints a list of all playlists, in the form:
-// p01: Coding Music - 2 tracks
-// p02: Other Playlist - 1 tracks
-const printPlaylists = function() {
-
+function printPlaylists() {
+       for (const playlistId in library.playlists) {
+              const playlist = library.playlists[playlistId];
+              console.log(`${playlist.id}: ${playlist.name} - ${playlist.tracks.length} tracks`);
+       }
 }
 
-
-// prints a list of all tracks, using the following format:
-// t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
-// t02: Model View Controller by James Dempsey (WWDC 2003)
-// t03: Four Thirty-Three by John Cage (Woodstock 1952)
-const printTracks = function() {
-
+function printTracks() {
+       for (const trackId in library.tracks) {
+              const track = library.tracks[trackId];
+              console.log(`${track.id}: ${track.name} by ${track.artist} (${track.album})`);
+       }
 }
 
-
-// prints a list of tracks for a given playlist, using the following format:
-// p01: Coding Music - 2 tracks
-// t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
-// t02: Model View Controller by James Dempsey (WWDC 2003)
-const printPlaylist = function(playlistId) {
-
+function printPlaylist(playlistId) {
+       const playlist = library.playlists[playlistId];
+       console.log(`${playlist.id}: ${playlist.name}`);
+       for (const trackId of playlist.tracks) {
+              const track = library.tracks[trackId];
+              console.log(`  ${track.id}: ${track.name} by ${track.artist} (${track.album})`);
+       }
 }
 
-
-// adds an existing track to an existing playlist
-const addTrackToPlaylist = function(trackId, playlistId) {
-
+function addTrackToPlaylist(trackId, playlistId) {
+       const playlist = library.playlists[playlistId];
+       if (playlist) {
+              if (!playlist.tracks.includes(trackId)) {
+                     playlist.tracks.push(trackId);
+                     console.log(`Track with ID ${trackId} added to playlist with ID ${playlistId}`);
+              } else {
+                     console.log(`Track with ID ${trackId} already exists in playlist with ID ${playlistId}`);
+              }
+       } else {
+              console.log(`Playlist with ID ${playlistId} does not exist.`);
+       }
 }
 
-
-// generates a unique id
-// (already implemented: use this for addTrack and addPlaylist)
-const generateUid = function() {
-  return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+function addTrack(track) {
+       const newTrackId = `t${Object.keys(library.tracks).length + 1}`;
+       track.id = newTrackId;
+       library.tracks[newTrackId] = track;
+       console.log(`Track with ID ${newTrackId} added to the library.`);
 }
 
-
-// adds a track to the library
-const addTrack = function(name, artist, album) {
-
+function addPlaylist(playlist) {
+       const newPlaylistId = `p${Object.keys(library.playlists).length + 1}`;
+       playlist.id = newPlaylistId;
+       library.playlists[newPlaylistId] = playlist;
+       console.log(`Playlist with ID ${newPlaylistId} added to the library.`);
 }
 
-
-// adds a playlist to the library
-const addPlaylist = function(name) {
-
+function printSearchResults(query) {
+       console.log(`Search results for "${query}":`);
+       for (const trackId in library.tracks) {
+              const track = library.tracks[trackId];
+              if (
+                     track.name.toLowerCase().includes(query.toLowerCase()) ||
+                     track.artist.toLowerCase().includes(query.toLowerCase()) ||
+                     track.album.toLowerCase().includes(query.toLowerCase())
+              ) {
+                     console.log(`${track.id}: ${track.name} by ${track.artist} (${track.album})`);
+              }
+       }
 }
 
+// Testing the functions
+console.log("All Playlists:");
+printPlaylists();
 
-// STRETCH:
-// given a query string string, prints a list of tracks
-// where the name, artist or album contains the query string (case insensitive)
-// tip: use "string".search("tri") 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/search
-const printSearchResults = function(query) {
+console.log("\nAll Tracks:");
+printTracks();
 
-}
+console.log("\nPlaylist p01:");
+printPlaylist("p01");
+
+console.log("\nAdding track t03 to playlist p01:");
+addTrackToPlaylist("t03", "p01");
+
+console.log("\nAdding a new track:");
+const newTrack = {
+       name: "New Track",
+       artist: "New Artist",
+       album: "New Album"
+};
+addTrack(newTrack);
+
+console.log("\nAdding a new playlist:");
+const newPlaylist = {
+       name: "New Playlist",
+       tracks: ["t01", "t03"]
+};
+addPlaylist(newPlaylist);
+
+console.log("\nSearch results:");
+printSearchResults("Code");
